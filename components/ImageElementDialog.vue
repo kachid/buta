fe0ce490648016fd20ec3dd4410842efdd22f712
Mikhail Kakhidze <kachid76@gmail.com>
2020-01-31 12:11:12 +0500

    add carousel into ImageElementDialog.vue

--- components/ImageElementDialog.vue
+++ components/ImageElementDialog.vue
@@ -4,33 +4,51 @@
     max-width="500"
     @click:outside="$emit('close_dialog')"
   >
-    <v-hover v-slot:default="{ hover }">
-      <v-card>
-        <v-img :src="src" max-width="500" contain>
-          <v-fab-transition>
-            <v-btn
-              v-if="hover"
-              color="white"
-              dark
-              absolute
-              top
-              right
-              fab
-              icon
-              outlined
-              class="mt-10"
-              @click="$emit('close_dialog')"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </v-fab-transition>
-        </v-img>
-      </v-card>
-    </v-hover>
+    <v-carousel
+      v-model="currentIndex"
+      hide-delimiters
+      hide-delimiter-background
+      show-arrows-on-hover
+      height="650"
+    >
+      <v-carousel-item v-for="(image, i) in images" :key="i">
+        <v-hover v-slot:default="{ hover }">
+          <v-card>
+            <v-img :src="image.src" max-width="500" contain>
+              <div>&nbsp;</div>
+              <v-expand-transition>
+                <div v-if="hover" class="gray lighten-5 text-center">
+                  {{ i + 1 }} of {{ images.length }}
+                </div>
+              </v-expand-transition>
+              <v-fab-transition>
+                <v-btn
+                  v-if="hover"
+                  color="white"
+                  dark
+                  absolute
+                  top
+                  right
+                  fab
+                  icon
+                  outlined
+                  class="mt-10"
+                  @click="$emit('close_dialog')"
+                >
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-fab-transition>
+            </v-img>
+          </v-card>
+        </v-hover>
+      </v-carousel-item>
+    </v-carousel>
   </v-dialog>
 </template>
 
 <script>
+import { images } from '@@/data'
+
 export default {
   name: 'ImageElementDialog',
   props: {
@@ -41,6 +59,25 @@ export default {
     dialog: {
       type: Boolean,
       default: false
+    }
+  },
+  data: () => ({
+    images,
+    currentIndex: 0
+  }),
+  computed: {
+    currentImageHeight() {
+      return this.getImageHeight(this.images[this.currentIndex].src)
+    }
+  },
+  mounted() {
+    this.currentIndex = this.images.findIndex((item) => item.src === this.src)
+  },
+  methods: {
+    getImageHeight(src) {
+      const img = new Image()
+      img.src = src
+      return img.height
     }
   }
 }
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import { images } from '@@/data'
 import AppImageElement from '@@/components/ImageElement'
 
 export default {
@@ -15,20 +16,7 @@ export default {
     AppImageElement
   },
   data: () => ({
-    images: [
-      { src: '/images/hotel_pool_02.jpg' },
-      { src: '/images/hotel_pool_03.jpg' },
-      { src: '/images/sunbeds.jpg' },
-      { src: '/images/sunbeds_02.jpg' },
-      { src: '/images/sunbeds_03.jpg' },
-      { src: '/images/sunbeds_04.jpg' },
-      { src: '/images/cafe.jpg' },
-      { src: '/images/cafe_02.jpg' },
-      { src: '/images/cafe_03.jpg' },
-      { src: '/images/bicycles.jpg' },
-      { src: '/images/bicycles_02.jpg' },
-      { src: '/images/bicycles_03.jpg' }
-    ]
+    images
   })
 }
 </script>
